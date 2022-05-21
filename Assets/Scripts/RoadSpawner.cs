eccffe6b8e46a374115501092f49be8fdfcffe9a
HamedHassani <hamed2008ir@gmail.com>
2022-05-21 19:02:31 +0430

    upload game

---
+++ Assets/Scripts/RoadSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadSpawner : MonoBehaviour
+{
+    public GameObject[] roads;
+    public GameObject block;
+
+    private float blockIndex, roadIndex=1;
+
+     void Start()
+    {
+        blockIndex = -10;
+        for (int i = 0; i < 10; i++)
+        {
+            SpawnBlock();
+        }
+        blockIndex = 0;
+    }
+    public void MakeRoad()
+    {
+        int n = Random.Range(0, roads.Length);
+        Vector3 newPosition = new Vector3(roadIndex * 100 - 100, 0f, 0f);
+        GameObject newRoad =
+            Instantiate(roads[n], newPosition , roads[n].transform.rotation);
+
+        for(int i = 0; i < 10; i++)
+        {
+            SpawnBlock();
+        }
+        roadIndex++;
+       
+    }
+    void SpawnBlock()
+    {
+        Vector3 newPosition = new Vector3(
+            Random.Range(blockIndex * 10 - 20, blockIndex * 10 - 10), 1f,
+            Random.Range(-1f, 1f));
+        GameObject newblock = Instantiate(block, newPosition, Quaternion.identity);
+        blockIndex++;
+    }
+}
---
+++ Assets/Scripts/Sphere.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    Rigidbody myBody;
+
+    public float gravity = 100f;
+    public float forwardSpeed = 10f;
+
+    public float moveZ;
+    public float moveSpeed = 10f;
+
+    private RoadSpawner roadSpawner;
+    
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody>();
+        //get road spawner
+        roadSpawner = GameObject.Find("Road Spawner").GetComponent<RoadSpawner>();
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            moveZ -= moveSpeed * Time.deltaTime;
+
+        }else if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            moveZ += moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            moveZ = 0f;
+        }
+      
+        if (transform.position.y < -3f)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(
+                UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+        }
+
+    }
+     void FixedUpdate()
+    {
+        myBody.velocity = new Vector3(forwardSpeed, 0f, moveZ);
+        myBody.AddForce(new Vector3(0f, -gravity, 0f) * myBody.mass);
+    }
+     void OnTriggerEnter(Collider target)
+    {
+        if (target.tag == "Create")
+        {
+            roadSpawner.MakeRoad();
+        }
+    }
+
+     void OnCollisionEnter(Collision target)
+    {
+        if (target.gameObject.tag == "Cube")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(
+    UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+
+        }
+    }
+
+
+}
